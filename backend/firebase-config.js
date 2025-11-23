df2671d3077e86f4b6672a44c3f12196fcc53c62
aloukikdas <aloukikdas6@gmail.com>
2025-11-23 23:25:10 +0530

    CHORE: Updated

--- backend/firebase-config.js
+++ backend/firebase-config.js
@@ -4,10 +4,12 @@ import { getDatabase } from "firebase/database";
 
 // ** Use placeholder text for now **
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY", 
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID", 
-  // ... other required keys
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
